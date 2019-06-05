775ef4e6a445dac112bff0694b18deafc101fd09
Mr Nezumi <kenjitsuchiya1@gmail.com>
2019-06-05 13:09:51 +0000

    Delete splitfiles.ps1

--- splitfiles.ps1
+++
@@ -1,9 +0,0 @@
-$sourceCSV = "pathxxxxx" ;
-$startrow = 0;
-$counter = 1 ;
-while ($startrow -lt 1188361)
-{
-Import-CSV $sourceCSV | select-object -skip $startrow -first 594180 | Export-CSV "pathxxxx($counter).csv" -NoClobber;
-$startrow += 0 ;
-$counter++ ;
- }
